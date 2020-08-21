60fec1db82df87ca48e1c2c0f180b25a5a84d9bd
Adebola <onedebos@gmail.com>
2020-08-21 11:22:20 +0100

    fix login form styles on mobile

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -24,14 +24,14 @@ const Login: React.FC<LoginProps> = ({
       <Menu isBtnDisabled={true} />
 
       <div className="min-h-auto flex flex-col justify-center items-center m-auto py-10">
-        <div className="w-3/4 md:w-3/4 lg:w-1/3">
+        <div className="w-5/6 md:w-3/4 lg:w-1/3">
           <div className="bg-blue-1001 text-white px-4 py-3 font-bold w-1/2 md:w-1/4 lg:w-1/4">
             <h2>Customer</h2>
           </div>
         </div>
         <form
           onSubmit={handleSubmit}
-          className="border-t-4 border-blue-1001 shadow-lg px-10 py-20 pb-32 w-3/4 lg:w-1/3"
+          className="border-t-4 border-blue-1001 shadow-lg px-6 md:px-10 py-20 pb-32 w-5/6 md:w-3/4 lg:w-1/3"
         >
           <div className="mb-8 text-blue-1001 ml-10">Lorem Ipsum</div>
 
@@ -39,7 +39,7 @@ const Login: React.FC<LoginProps> = ({
           <div>
             <input
               type="email"
-              className="border border-gray-500 p-2 rounded-md mb-6 w-full"
+              className="border border-gray-500 p-2 rounded-md mb-6 w-full mt-2"
               onChange={handleEmailChange}
               required
             />
@@ -48,7 +48,7 @@ const Login: React.FC<LoginProps> = ({
           <div>
             <input
               type="password"
-              className="border border-gray-500 p-2 rounded-md w-full"
+              className="border border-gray-500 p-2 rounded-md w-full mt-2"
               onChange={handlePasswordChange}
               required
             />
